031925d71117ad123ed72bcc5caddecf756d2bb3
Keerthana B <60786451+Keerthanab14@users.noreply.github.com>
2021-09-08 02:48:36 +0530

    Update UserRepository.java

--- src/main/java/com/example/synergybackend/repository/UserRepository.java
+++ src/main/java/com/example/synergybackend/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.synergybackend.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.synergybackend.model.OpenEnded;
 import com.example.synergybackend.model.User;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User,Long> {
-
+public interface UserRepository extends MongoRepository<User, String> {
+    public User findByGoogleId(String googleId);
 }
